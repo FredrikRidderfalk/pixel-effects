ea551a7d61e5682081a83b42e8bfafef509c2474
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-03-31 17:12:04 -0400

    created custom gray filter for images

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ canvas.width = 800;
 canvas.height = 450;
 
 const image1 = new Image();
-image1.src = "image1.png";
+image1.src = "images/futureworks.png";
+
+// This event listener turns
+image1.addEventListener("load", function () {
+  ctx.drawImage(image1, 0, 0, canvas.width, canvas.height); // If the image is the same size as the canvas, the drawImage function only needs the first two arguments.
+  const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  console.log("scannedImage", scannedImage); // Here we find the array data containing the rgba color of each pixel, defined as a number between 0 and 255, even the alpha value that usually has a value between 0 and 1, not 0 and 255. The first value defines the r(red), the second the g(green), the third the b(blue), and the forth the a(alpha).
+  const scannedData = scannedImage.data;
+  for (let i = 0; i < scannedData.length; i += 4) {
+    const total = scannedData[i] + scannedData[i + 1] + scannedData[i + 2];
+    const averageColorValue = total / 3;
+    scannedData[i] = averageColorValue; //add + 154 to averageColorValue to change the color it will have to bright red.
+    scannedData[i + 1] = averageColorValue;
+    scannedData[i + 2] = averageColorValue;
+  }
+  scannedImage.data = scannedData;
+  ctx.putImageData(scannedImage, 0, 0);
+});
