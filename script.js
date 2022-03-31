9e6612f2c24bc3eda82775da6f6e5327aea843e7
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-03-31 15:48:38 -0400

    added constants and defined canvas dimensions

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("#canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 450;
+
+const image1 = new Image();
+image1.src = "image1.png";
