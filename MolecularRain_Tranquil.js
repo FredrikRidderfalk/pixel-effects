42f0248a6db4f3e8afc55398fb888f6c607d4154
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-04-07 15:33:43 -0400

    style: comment out globalAlpha

--- MolecularRain_Tranquil.js
+++ MolecularRain_Tranquil.js
@@ -3,7 +3,7 @@
 // Here we create arc paths for the particles with some trigonometry
 
 const myImage = new Image();
-myImage.src = "images/ds-tranquil.jpg";
+myImage.src = "images/lofi.jpg";
 
 // This event listener applies a snowy rain to myImage
 myImage.addEventListener("load", function () {
@@ -48,10 +48,10 @@ myImage.addEventListener("load", function () {
   class Particle {
     constructor() {
       this.x = Math.random() * canvas.width;
-      this.y = 0;
+      this.y = Math.random() * canvas.height;
       this.speed = 0;
       this.velocity = Math.random() * 0.5;
-      this.size = Math.random() * 1.5 + 0.5;
+      this.size = Math.random() * 3.5 + 0.5;
       this.position1 = Math.floor(this.y); // this is because array indexes need to be integers, not floats
       this.position2 = Math.floor(this.x); // this is because array indexes need to be integers, not floats
       this.angle = 0; // We want each particle to fluctuate depending on the brightness of the underlying area in the image
@@ -67,11 +67,11 @@ myImage.addEventListener("load", function () {
         this.speed = mappedImage[this.position1][this.position2][0]; // relative brightness is now reflected in this.speed
       } // This if statement makes sure that we only assign a speed if a particle is within the canvas area
       let movement = 2.55 - this.speed + this.velocity; // relative brightness is a number between 0 and 2.55. We want the dark particles with a value close to 0 move really fast, we take the maximum value of 2.55 and subtract this.speed. We also don't want particles with the same color to move exactly the same speed, so we randomize it somewhat by adding this.velocity.
-      this.angle += this.speed / 20; // set it to ++ (+=1) for a vibration effect. The lower the value, the more clear we can see trailing circles being drawn on the canvas. 0.2 draws clear circles. We can also set it to this.speed for the speed to vary depending on how bright the underlying background is.
+      //   this.angle += this.speed / 20; // set it to ++ (+=1) for a vibration effect. The lower the value, the more clear we can see trailing circles being drawn on the canvas. 0.2 draws clear circles. We can also set it to this.speed for the speed to vary depending on how bright the underlying background is.
 
       //   this.y += this.velocity; // this creates snow/rain particles that doesn't interact with each other
-      this.y += movement + Math.sin(this.angle) * 3; // change this for more/less wiggle effect
-      this.x += movement + Math.cos(this.angle) * 3; // change this for more/less wiggle effect
+      this.y += movement / 5; // change this for more/less wiggle effect
+      this.x += movement / 2; // change this for more/less wiggle effect
       if (this.y >= canvas.height) {
         this.y = 0;
         this.x = Math.random() * canvas.width;
@@ -83,12 +83,11 @@ myImage.addEventListener("load", function () {
     }
     draw() {
       ctx.beginPath();
-      //   ctx.fillStyle = "white";
       if (
         mappedImage[this.position1] &&
         mappedImage[this.position1][this.position2]
       ) {
-        ctx.fillStyle = mappedImage[this.position1][this.position2][1]; // in the very end we chose index position 1 instead of 0 since cellColor is the second item in the cell array. This makes us draw in color. Comment out and use "white" instead if desiring grayscale
+        ctx.fillStyle = mappedImage[this.position1][this.position2][1];
       } // This if statement makes sure that we only assign a change the color of a particle if that particle is within the canvas area
       ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
       ctx.fill();
@@ -104,14 +103,14 @@ myImage.addEventListener("load", function () {
     //ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height); // comment this out to make the image invisible, which creates a wonderful effect together with the particle waterfall effect
 
     // the next 3 lines give particles fade-in trails
-    ctx.globalAlpha = 0.05;
+    // ctx.globalAlpha = 0.05;
     ctx.fillStyle = "rgb(0, 0, 0)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.globalAlpha = 0.2; // if we add this here with a new value, we change the fade-in trails after the image has been drawn
+    // ctx.globalAlpha = 0.2; // if we add this here with a new value, we change the fade-in trails after the image has been drawn
     for (let i = 0; i < particlesArray.length; i++) {
       particlesArray[i].update();
-      ctx.globalAlpha = particlesArray[i].speed * 0.5; // Change the value 0.5 to change the speed. Comment this out to maintain a more consistent waterfall effect over the whole image
+      //   ctx.globalAlpha = particlesArray[i].speed * 0.5; // Change the value 0.5 to change the speed. Comment this out to maintain a more consistent waterfall effect over the whole image
       particlesArray[i].draw();
     }
     requestAnimationFrame(animate);
